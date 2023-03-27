ba226c5f3a75a863ef129c1f3f77693a31cf263d
Hilal Oruç <hiilaloruc@gmail.com>
2023-03-27 13:35:32 +0300

    ui design for Room completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Room from "./components/Room";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="App comfortaa-font">
+      <Room />
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import GoogleFonts from "google-fonts";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+GoogleFonts.add({
+  Comfortaa: "400",
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
